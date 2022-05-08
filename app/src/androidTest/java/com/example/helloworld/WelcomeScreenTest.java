685a3198ac5a13ac0eeb8120e1754c1aadd069b5
VictorHugo <victorhugo@VictorHugos-MacBook-Pro.local>
2022-05-07 18:57:50 -0700

    added test to welcomeScreen

--- app/src/androidTest/java/com/example/helloworld/WelcomeScreenTest.java
+++ app/src/androidTest/java/com/example/helloworld/WelcomeScreenTest.java
@@ -1,12 +1,37 @@
 package com.example.helloworld;
 
-import junit.framework.TestCase;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-public class WelcomeScreenTest extends TestCase {
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-    public void testOnCreate() {
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class WelcomeScreenTest {
+    @Rule
+    public ActivityScenarioRule<WelcomeScreen> activityTestRule
+            = new ActivityScenarioRule<>(WelcomeScreen.class);
+
+    @Test
+    public void succesfulNavigationToSettings(){
+        onView(withContentDescription(R.string.drawer_open)).perform(click());
+        onView(withId(R.id.settings_menu_item)).perform(click());
+        onView(withId(R.id.settingsTextView)).check(matches(withText("Settings")));
     }
 
-    public void testOnBackClick() {
+    @Test
+    public void succesfulNavigationToMatches(){
+        onView(withContentDescription(R.string.drawer_open)).perform(click());
+        onView(withId(R.id.matches_menu_item)).perform(click());
+        onView(withId(R.id.matchesTextView)).check(matches(withText("Matches")));
     }
 }
